cc48ca01d18eeb605d906a7306d624fa3d2c591c
vibhor <vibhorsmiki@gmail.com>
2020-07-30 17:38:31 +0530

    init

--- assignments/week04/day04.py
+++ assignments/week04/day04.py
@@ -1,40 +1,38 @@
-# def bSearch(array, target):
-#   return bSearchHelperRecursive(array, target, 0, len(array)-1 )
-# #   return bSearchHelperIterative(array, target, 0, len(array)-1 )
+def bSearch(array, target):
+  return bSearchHelperRecursive(array, target, 0, len(array)-1 )
+#   return bSearchHelperIterative(array, target, 0, len(array)-1 )
 
-# def bSearchHelperRecursive(array, target, left, right):
-#   if left > right:
-#     return -1
+def bSearchHelperRecursive(array, target, left, right):
+  if left > right:
+    return -1
   
-#   middle = (left+right)//2
-#   potentialMatch = array[middle]
+  middle = (left+right)//2
+  potentialMatch = array[middle]
 
-#   if target == potentialMatch:
-#     return middle
-#   elif target < potentialMatch:
-#     return bSearchHelperRecursive(array, target, left, middle-1)
-#   elif target > potentialMatch:
-#     return bSearchHelperRecursive(array, target, middle+1, right)
+  if target == potentialMatch:
+    return middle
+  elif target < potentialMatch:
+    return bSearchHelperRecursive(array, target, left, middle-1)
+  elif target > potentialMatch:
+    return bSearchHelperRecursive(array, target, middle+1, right)
 
-# def bSearchHelperIterative(array, target, left, right):
-#   while left <= right:
+def bSearchHelperIterative(array, target, left, right):
+  while left <= right:
 
-#     middle = (left+right)//2
-#     potentialMatch = array[middle]
+    middle = (left+right)//2
+    potentialMatch = array[middle]
 
-#     if target == potentialMatch:
-#       return middle
-#     elif target < potentialMatch:
-#       right = middle-1      
-#     elif target > potentialMatch:
-#       left = middle+1
+    if target == potentialMatch:
+      return middle
+    elif target < potentialMatch:
+      right = middle-1      
+    elif target > potentialMatch:
+      left = middle+1
       
 
-#   return -1
+  return -1
 
-# # driver
-# arr = [1,2,3,4,5,6,7,8]
-# print(bSearch(arr, 8))
+# driver
+arr = [1,2,3,4,5,6,7,8]
+print(bSearch(arr, 8))
 
-l = [1,2,43,6,43,6,5]
-print(l)
